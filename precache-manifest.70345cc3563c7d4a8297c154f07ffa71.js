b19beeec2b5383c1403d367479bb044bfb2b0d61
Fredrik Hammar <fhammar@volvocars.com>
2023-08-16 22:22:47 +0200

    Updates

---
+++ precache-manifest.70345cc3563c7d4a8297c154f07ffa71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b00290780ec7d7f4d443",
+    "url": "/weddingsite/static/css/main.773068ca.chunk.css"
+  },
+  {
+    "revision": "b00290780ec7d7f4d443",
+    "url": "/weddingsite/static/js/main.b0029078.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/weddingsite/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/weddingsite/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "952c086989eef9102a82",
+    "url": "/weddingsite/static/js/runtime~main.952c0869.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/weddingsite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c4d46f14d2f1fe268f6dbddbff0d86c6",
+    "url": "/weddingsite/index.html"
+  }
+];

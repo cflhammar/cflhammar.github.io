d95b36b35e444d4ccc6d6994a8f5dcce43f8cedd
Fredrik Hammar <fhammar@volvocars.com>
2023-08-17 12:01:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weddingsite/precache-manifest.37203f30ddf2e6454d0e5a4b641283f2.js"
+  "/precache-manifest.e0bfb4ecf73c3d6f1df7f5959e068f0a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weddingsite/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

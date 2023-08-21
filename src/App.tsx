cb7cecbae530b3f1b8ea111b3efdab801edc3189
Fredrik Hammar <fhammar@volvocars.com>
2023-08-21 20:11:21 +0200

    adds login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import StartPage from "./components/StartPage";
 import Program from "./components/Program";
@@ -9,27 +9,56 @@ import Boende from "./components/Boende";
 import Info from "./components/Info";
 import Filipstad from "./components/Filipstad";
 import Footer from "./components/Footer/Footer";
+import {Button} from "@material-ui/core";
 
 function App() {
     const [pageId, setPageId] = useState(0);
+    const [password, setPassword] = useState("");
+    const [isVerified, setIsVerified] = useState(false);
 
-  return (
-    <div className="App">
-        <Router >
-            <Header pageId={pageId}  setPageId={setPageId}/>
-            <div className="text-container">
-                <Routes>
-                    <Route path="/" element={<StartPage />} />
-                    <Route path="/program" element={<Program />} />
-                    <Route path="/boende" element={<Boende />} />
-                    <Route path="/ovrig-info" element={<Info />} />
-                    <Route path="/filipstad" element={<Filipstad />} />
-                </Routes>
+    const handleChange = (event: { target: { value: React.SetStateAction<string> | null; }; }) => {
+        // 👇 Get input value from "event"
+        if (event.target.value != null)  setPassword(event.target.value);
+    };
+
+    const validate = () => {
+        if (password === "pw") setIsVerified(true)
+    }
+
+    if (isVerified) {
+        return (
+            <div className="App">
+                <Router>
+                    <Header pageId={pageId} setPageId={setPageId}/>
+                    <div className="text-container">
+                        <Routes>
+                            <Route path="/" element={<StartPage/>}/>
+                            <Route path="/program" element={<Program/>}/>
+                            <Route path="/boende" element={<Boende/>}/>
+                            <Route path="/ovrig-info" element={<Info/>}/>
+                            <Route path="/filipstad" element={<Filipstad/>}/>
+                        </Routes>
+                    </div>
+                    <Footer pageId={pageId}/>
+                </Router>
             </div>
-            <Footer  pageId={pageId}/>
-        </Router>
-    </div>
-  );
+        )}
+    else {
+        return (
+            <div className="login">
+                <div>
+                    <p className="helper">Fyll i kod:</p>
+                <input
+                    type="text"
+                    onChange={handleChange}
+                />
+                </div>
+                <div className="login-button">
+                <Button onClick={() => validate()} variant="contained">Klicka här!!!</Button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;

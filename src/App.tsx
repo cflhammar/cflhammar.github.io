4dd7c7f1149eb34cd3b9201ed959a26f81014099
Fredrik Hammar <fhammar@volvocars.com>
2023-09-05 17:06:56 +0200

    logout default

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {Button} from "@material-ui/core";
 function App() {
     const [pageId, setPageId] = useState(0);
     const [password, setPassword] = useState("");
-    const [isVerified, setIsVerified] = useState(true);
+    const [isVerified, setIsVerified] = useState(false);
 
     const handleChange = (event: { target: { value: React.SetStateAction<string> | null; }; }) => {
         if (event.target.value != null)  setPassword(event.target.value);
